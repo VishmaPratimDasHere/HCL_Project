1f5ee58dd93153ca04be890d22824e6de48a7f55
vishmapratimdashere <bhishmadas9@gmail.com>
2025-11-14 22:26:24 -0500

    Added tailwind css packages into dev dependencies

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app=express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.get("/", (req, res) => {
+    res.status(200).send({Message: "Working fine"})
+    // may show home.ejs later
+})
+
 app.listen(3000, () => {
     console.log("Listening on port 3000")
 })
