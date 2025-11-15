a1d32079fab4938d7320bb120224f6a3f6c5e822
vishmapratimdashere <bhishmadas9@gmail.com>
2025-11-14 22:08:02 -0500

    Added basic express server code

---
+++ app.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const path=require(path)
+
+const app=express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
