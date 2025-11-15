c5df38c81369bc6f359eee171b6b55112b1e2d7b
vishmapratimdashere <bhishmadas9@gmail.com>
2025-11-14 23:15:36 -0500

    Some navbars and stuff

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express=require('express')
-const path=require('path')
+const express = require('express')
+const path = require('path')
 
-const app=express()
+const app = express()
 
 app.set('view engine', 'ejs')
+app.set('views', 'views')
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-    res.status(200).send({Message: "Working fine"})
+    // res.status(200).json({
+    //     Message: "Working fine",
+    //     Data: "Great job"
+    // })
+    res.render("home")
     // may show home.ejs later
 })
 
